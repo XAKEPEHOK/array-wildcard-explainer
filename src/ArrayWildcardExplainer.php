7f8f0f4d6ba05a2eeba3cc7acc779c98705574ca
XAKEPEHOK <icq-475204@ya.ru>
2023-01-07 02:20:26 +0300

    Initial commit

---
+++ src/ArrayWildcardExplainer.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Date: 13.11.2020
+ * @author Timur Kasumov (XAKEPEHOK)
+ */
+
+namespace XAKEPEHOK\ArrayWildcardExplainer;
+
+
+class ArrayWildcardExplainer
+{
+    public static function explainOne(array $data, string $key, bool $inverse = false): array
+    {
+        $result = self::explain($data, $key, $inverse);
+        sort($result);
+        return array_values($result);
+    }
+
+    public static function explainMany(array $data, array $keys, bool $inverse = false): array
+    {
+        $result = [];
+        foreach ($keys as $key) {
+            $result = array_merge($result, self::explain($data, $key, $inverse));
+        }
+
+        sort($result);
+        return array_values($result);
+    }
+
+    private static function explain(array $data, string $key, bool $inverse = false): array
+    {
+        $flat = array_keys(self::flatten($data));
+
+        $extended = [];
+        foreach ($flat as $path) {
+            $extended[$path] = $path;
+            $elements = explode('.', $path);
+            while (array_pop($elements)) {
+                $value = implode('.', $elements);
+                $extended[$value] = $value;
+            }
+        }
+
+        $regex = implode('[^.]+', array_map(
+            function ($value) {
+                return preg_quote($value, '~');
+            },
+            explode('*', $key)
+        ));
+        $regex = "~^{$regex}$~";
+
+        return array_filter($extended, function ($value) use ($regex, $inverse) {
+            if ($inverse) {
+                return !preg_match($regex, $value);
+            }
+            return preg_match($regex, $value);
+        });
+    }
+
+    private static function flatten(array $items, $prepend = ''): array
+    {
+        $flatten = [];
+        foreach ($items as $key => $value) {
+            if (is_array($value) && !empty($value)) {
+                $flatten = array_merge(
+                    $flatten,
+                    self::flatten($value, $prepend . $key . '.')
+                );
+            } else {
+                $flatten[$prepend . $key] = $value;
+            }
+        }
+
+        return $flatten;
+    }
+
+}
---
+++ tests/ArrayWildcardExplainerTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace XAKEPEHOK\ArrayWildcardExplainer;
+
+use PHPUnit\Framework\TestCase;
+
+class ArrayWildcardExplainerTest extends TestCase
+{
+
+    private array $data;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->data = [
+            'direct' => 1,
+            'nested' => [
+                'value_1' => 11,
+                'value_2' => 22,
+                'value_3' => [
+                    ['value' => 111],
+                    ['value' => 222],
+                ],
+            ],
+            'array' => [
+                [
+                    'value_1' => 1111,
+                    'value_2' => 2222,
+                ],
+                [
+                    'value_1' => 1111,
+                    'value_2' => 2222,
+                ],
+                [
+                    'value_1' => 1111,
+                    'value_2' => 2222,
+                    'value_3' => [
+                        [
+                            'value_1' => 11111,
+                            'value_2' => 22222,
+                        ],
+                        [
+                            'value_1' => 11111,
+                            'value_2' => 22222,
+                        ],
+                    ],
+                ],
+                [
+                    'value_1' => 11111,
+                    'value_2' => 22222,
+                    'value_3' => [
+                        [
+                            'value_1' => 111111,
+                            'value_2' => 222222,
+                        ],
+                        [
+                            'value_1' => 111111,
+                            'value_2' => 222222,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function expandDataProvider(): array
+    {
+        $data = [
+            [
+                'direct',
+                [
+                    'direct',
+                ]
+            ],
+            [
+                'nested.*',
+                [
+                    'nested.value_1',
+                    'nested.value_2',
+                    'nested.value_3',
+                ]
+            ],
+            [
+                'nested.value_3',
+                [
+                    'nested.value_3',
+                ]
+            ],
+            [
+                'nested.value_3.*',
+                [
+                    'nested.value_3.0',
+                    'nested.value_3.1',
+                ]
+            ],
+            [
+                'nested.*.*.value',
+                [
+                    'nested.value_3.0.value',
+                    'nested.value_3.1.value',
+                ]
+            ],
+            [
+                'array.*.value_3',
+                [
+                    'array.2.value_3',
+                    'array.3.value_3',
+                ]
+            ],
+            [
+                'array.*.value_3.*.value_1',
+                [
+                    'array.2.value_3.0.value_1',
+                    'array.2.value_3.1.value_1',
+                    'array.3.value_3.0.value_1',
+                    'array.3.value_3.1.value_1',
+                ]
+            ],
+        ];
+
+        return array_combine(array_column($data, 0), $data);
+    }
+
+    /**
+     * @dataProvider expandDataProvider
+     * @param $input
+     * @param $expected
+     */
+    public function testExpandOne($input, $expected)
+    {
+        $this->assertEquals($expected, ArrayWildcardExplainer::explainOne($this->data, $input));
+    }
+}
